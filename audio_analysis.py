a953a872c1a9e7d335f43e80dfa92a05a934640f
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-03-24 17:19:35 +0530

    Update audio_analysis.py

--- audio_analysis.py
+++ audio_analysis.py
@@ -28,27 +28,48 @@ def audio_waveframe(file_path):
     return plot
 
 
+# def spectrogram(file_path):
+#     # Compute the short-time Fourier transform (STFT)
+#     n_fft = 500  # Number of FFT points 2048
+#     hop_length = 1  # Hop length for STFT 512
+#     audio_data, sampling_rate = librosa.load(file_path)
+#     stft = librosa.stft(audio_data, n_fft=n_fft, hop_length=hop_length)
+#     # Convert the magnitude spectrogram to decibels (log scale)
+#     spectrogram = librosa.amplitude_to_db(np.abs(stft))
+#     # Plot the spectrogram
+#     plt.figure(figsize=(30, 10))
+#     # librosa.display.specshow(spectrogram, sr=sampling_rate, hop_length=hop_length, x_axis='time', y_axis='linear')
+#     librosa.display.specshow(spectrogram, sr=sampling_rate, hop_length=hop_length)
+#     plt.colorbar(format='%+2.0f dB')
+#     plt.title('Spectrogram')
+#     plt.xlabel('Time (s)')
+#     plt.ylabel('Frequency (Hz)')
+#     plt.tight_layout()
+#     # plt.savefig('spectrogram.png')
+#     plot = st.pyplot(plt)
+
+#     return plot
+
+
 def spectrogram(file_path):
-    # Compute the short-time Fourier transform (STFT)
-    n_fft = 500  # Number of FFT points 2048
-    hop_length = 1  # Hop length for STFT 512
-    audio_data, sampling_rate = librosa.load(file_path)
-    stft = librosa.stft(audio_data, n_fft=n_fft, hop_length=hop_length)
-    # Convert the magnitude spectrogram to decibels (log scale)
-    spectrogram = librosa.amplitude_to_db(np.abs(stft))
+    
+    y, sr = librosa.load(file_path)
+    # Compute the spectrogram
+    D = librosa.stft(y)
+    # Convert magnitude spectrogram to decibels
+    DB = librosa.amplitude_to_db(abs(D))
     # Plot the spectrogram
-    plt.figure(figsize=(30, 10))
-    # librosa.display.specshow(spectrogram, sr=sampling_rate, hop_length=hop_length, x_axis='time', y_axis='linear')
-    librosa.display.specshow(spectrogram, sr=sampling_rate, hop_length=hop_length)
+    plt.figure(figsize=(10, 5))
+    librosa.display.specshow(DB, sr=sr, x_axis='time', y_axis='hz')
     plt.colorbar(format='%+2.0f dB')
     plt.title('Spectrogram')
     plt.xlabel('Time (s)')
     plt.ylabel('Frequency (Hz)')
     plt.tight_layout()
-    # plt.savefig('spectrogram.png')
     plot = st.pyplot(plt)
 
     return plot
+
 
 
 def audio_signals(file_path):
